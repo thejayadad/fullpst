1a1663abbc9c9f28b325879530e55a5cf8cc6c98
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-04 17:49:47 -0400

    update

--- front/src/components/Sidebar.js
+++ front/src/components/Sidebar.js
@@ -4,17 +4,36 @@ const Sidebar = () => {
   return (
     <section className='h-full w-1/5 flex flex-col justify-center sticky top-10 left-0'>
         <div className='h-96 flex justify-center sticky top-4 left-0'>
-            <ul className='flex flex-col justify-between '>
-                <li className='bg-sky-100 p-6 m-2 w-24'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-                <li className='bg-sky-100 p-6 m-2'>All</li>
-             </ul>
-        </div>
+        <ul className="p-6 divide-y divide-slate-200 align-center">
+
+    <li className="flex py-4 first:pt-0 last:pb-0 items-center	">
+      <img class="h-10 w-10 rounded-full" src="https://images.pexels.com/photos/1426620/pexels-photo-1426620.jpeg" alt="category" />
+      <div class="ml-3 overflow-hidden">
+        <p class="text-sm font-medium text-slate-900">Category</p>
+      </div>
+    </li>
+    <li className="flex py-4 first:pt-0 last:pb-0 items-center	">
+      <img class="h-10 w-10 rounded-full" src="https://images.pexels.com/photos/1426620/pexels-photo-1426620.jpeg" alt="category" />
+      <div class="ml-3 overflow-hidden">
+        <p class="text-sm font-medium text-slate-900">Category</p>
+      </div>
+    </li>
+    <li className="flex py-4 first:pt-0 last:pb-0 items-center	">
+      <img class="h-10 w-10 rounded-full" src="https://images.pexels.com/photos/1426620/pexels-photo-1426620.jpeg" alt="category" />
+      <div class="ml-3 overflow-hidden">
+        <p class="text-sm font-medium text-slate-900">Category</p>
+      </div>
+    </li>
+    <li className="flex py-4 first:pt-0 last:pb-0 items-center	">
+      <img class="h-10 w-10 rounded-full" src="https://images.pexels.com/photos/1426620/pexels-photo-1426620.jpeg" alt="category" />
+      <div class="ml-3 overflow-hidden">
+        <p class="text-sm font-medium text-slate-900 ">Category</p>
+      </div>
+    </li>
+
+</ul>
+
+    </div>
 
 
     </section>
