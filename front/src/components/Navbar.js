c448790d905491c8abb36bc7bf2f3d8a90aee902
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-04 17:36:29 -0400

    update

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className='w-full bg-sky-400 p-6'>
-        Header
+    <header className='w-full bg-sky-400 p-4 sticky top-0 flex justify-between'>
+        <div>LOGO</div>
+        <div>LOGO</div>
+        <div>LOGO</div>
+
     </header>
   )
 }
--- front/src/components/Sidebar.js
+++ front/src/components/Sidebar.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <section className='h-full w-1/5 flex justify-center bg-slate-100 p-5'>
-        sidebar
+    <section className='h-full w-1/5 flex flex-col justify-center sticky top-10 left-0'>
+        <div className='h-96 flex justify-center sticky top-4 left-0'>
+            <ul className='flex flex-col justify-between '>
+                <li className='bg-sky-100 p-6 m-2 w-24'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+                <li className='bg-sky-100 p-6 m-2'>All</li>
+             </ul>
+        </div>
+
+
     </section>
   )
 }
